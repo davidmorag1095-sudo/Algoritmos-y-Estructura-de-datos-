f93b2d4d1ab02d12fd05f137fca33c744eed1192
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-03-19 08:40:07 -0600

    asda

---
+++ Nodos/Nodo.java
@@ -0,0 +1,26 @@
+public class Nodo
+{
+	int datos;
+	Nodo siguiente;
+#--------------------------------------------------
+	#Set/Gets 
+	public void setDatos(int datos)
+	{
+		this.datos = datos;
+	}
+	public void setSiguiente(Nodo siguiente)
+	{
+		this.siguiente = siguiente;
+	}
+	#--------------------------------------------------
+	public int getDatos()
+	{
+		return datos
+	}
+	public int getSiguiente()
+	{
+		return siguiente;
+	}
+}	//Fin de la clase 
+	
+	
