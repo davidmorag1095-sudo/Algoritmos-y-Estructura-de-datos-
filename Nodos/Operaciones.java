588c434f21bb1c465ad22a9bed5cbdd9c85a6291
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-03-19 09:24:00 -0600

    asd

---
+++ Nodos/Operaciones.java
@@ -0,0 +1,59 @@
+public class Operaciones
+{
+	Nodo primero, ultimo, nuevo, rec;
+
+	public void agregarAlInicio(int datos)
+	{
+		//Se crea el nuevo Nodo
+		nuevo = new Nodo(datos);
+		
+		//Validacion para verificar que el primero no este vacio 
+		if (primero == null)
+		{
+			primero = nuevo;
+			ultimo = nuevo;
+		}else
+		{
+			nuevo.setSiguiente(primero);
+			primero = nuevo
+		}
+	}//Fin del agregar al inicio
+	//--------------------------------------------------------------
+	public void agregarAlFinal(int datos)
+	{
+		nuevo = new Nodo(datos);
+		if(primero == null)
+		{
+			primero = nuevo;
+			ultimo = nuevo;
+		}
+		else
+		{
+			ultimo.setSiguiente(nuevo);
+			ultimo = nuevo
+		}
+		
+	}//Fin del agregar al final
+	 //--------------------------------------------------------------
+	 public String imprimir()
+	 {
+		String mensaje = "";
+		
+		return mensaje;
+	 }//Fin del imprimir 
+	 //--------------------------------------------------------------
+	 public String agregarDespues(int datos, int buscar)
+	 {
+		String mensaje = "";
+		
+		return mensaje; 
+	 }//Fin de agregarDespues
+	 //--------------------------------------------------------------
+	 public String eliminar(int buscar)
+	 {
+		String mensaje = "";
+		
+		return mensaje = ""; 
+	 }//Fin del eliminar
+	 
+}//Fin de la clase
